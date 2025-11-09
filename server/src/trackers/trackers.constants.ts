7e620ea7730b677c483298eb2dc59e6034c23052
Balint Benedek <benedek.balint91@gmail.com>
2025-11-09 22:22:36 +0100

    Readme kiegészítése és elnevezés javítása

--- server/src/trackers/trackers.constants.ts
+++ server/src/trackers/trackers.constants.ts
@@ -3,7 +3,7 @@ import { TrackerOption } from './tracker.types';
 
 export const TRACKER_OPTIONS: TrackerOption[] = [
   { value: TrackerEnum.NCORE, label: 'nCore' },
-  { value: TrackerEnum.BITHUMEN, label: 'bitHUmen' },
+  { value: TrackerEnum.BITHUMEN, label: 'BitHUmen' },
 ];
 
 export const TRACKER_LABEL_MAP = TRACKER_OPTIONS.reduce(
