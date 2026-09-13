41c6986f409576fb75f3da3ecf02bda42845250b
Balint Benedek <benedek.balint91@gmail.com>
2026-09-13 11:36:00 +0200

    Frontend hibakazelés módosítása

--- client/src/shared/components/default-error.tsx
+++ client/src/shared/components/default-error.tsx
@@ -3,12 +3,14 @@ import { AlertCircleIcon } from 'lucide-react'
 import { Alert, AlertDescription, AlertTitle } from './ui/alert'
 import { Button } from './ui/button'
 
-type DefaultError = {
-  error: Error
+type DefaultErrorProps = {
+  error: unknown
 }
 
-export function DefaultError(props: DefaultError) {
+export function DefaultError(props: DefaultErrorProps) {
   const { error } = props
+
+  const errorMessage = error instanceof Error ? error.message : String(error)
 
   return (
     <div className="flex justify-center py-4">
@@ -18,7 +20,7 @@ export function DefaultError(props: DefaultError) {
           Hiba történt az StremHU Source betöltése közben!
         </AlertTitle>
         <AlertDescription>
-          <p className="font-mono">{error.message}</p>
+          <p className="font-mono">{errorMessage}</p>
           <div className="w-full flex justify-end">
             <Button
               variant="outline"
